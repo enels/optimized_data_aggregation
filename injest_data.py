03ce3ff0e254d1bcf6ae93f278f945b50ef18333
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-16 09:26:12 +0100

    add some comment on the main file for data ingestion to aid code understanding

--- injest_data.py
+++ injest_data.py
@@ -2,6 +2,18 @@ from data_class import LoadData
 
 if __name__ == "__main__":
     
+    """
+        the data is a list of csv files that is stored in the data variable
+        the program loops through the data list and collect each of the file
+        stored in the list index for error checking and validation.
+
+        All you have to do is make changes to the database connection file to match
+        what you have and add more files to the the data list object for iteration
+
+        But first, make sure you have created a database and create tables in it to match
+        the possible columns you may have in the csv file.
+    """
+
     data = "sales_data.csv"
 
     # create the object to load data into database
