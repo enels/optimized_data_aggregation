68a103888b891d3ea5c283ed6a2e6debd3b695ff
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-18 16:30:30 +0100

    store error(s) in a seperate errors dictionary

--- utilities/file_error_checker.py
+++ utilities/file_error_checker.py
@@ -4,56 +4,59 @@ import csv
 
 class FileErrorCheck:
 
-    def __init__(self, file_path):
+    def __init__(self, __file_path):
         
-        self.file_path = file_path
-        self.file_name = ""
+        self.__file_path = __file_path
+        self.__file_name = ""
+        self.__file_errors = dict()
 
     
-    def _extract_file_name(self):
+    def __extract_file_name(self):
 
         """
             Extracts the file name
         """
 
-        if "/" in self.file_path:
-            self.file_name = self.file_path.split("/")[-1]
+        if "/" in self.__file_path:
+            self.__file_name = self.__file_path.split("/")[-1]
         else:
-            self.file_name = self.file_path
+            self.__file_name = self.__file_path
 
     def get_file_name(self):
 
         # checks file for errors
-        self._check_file()
+        self.__check_file()
 
         # extract the file name from the file path
         # checks if file name is already set
-        if self.file_name == "":
-            self._extract_file_name()
+        if self.__file_name == "":
+            self.__extract_file_name()
         
         # return file name
-        return self.file_name
+        return self.__file_name
 
-    def _check_file(self):
+    def __check_file(self):
 
 
         try:
-            with open(self.file_path) as csv_file:
+            with open(self.__file_path) as csv_file:
 
                 csv_reader = csv.reader(csv_file, delimiter=',')
 
         except FileNotFoundError:
             self._extract_file_name()
-            print("Error: The csv file with name {0} cannot be found".format(self.file_name))
+            self.__file_errors["Error: The csv file with name {} cannot be found".format(self.__file_name)] = \
+            True
 
         # check for bad file format
         except csv.Error as e:
-            print("Error: {}".format(e))
+            self.__file_errors["Error: {}".format(e)] = True
 
         # check for characters that are outside the unicode system
         except UnicodeDecodeError:
-            print("Error: File contains illegal character")
+            self.__file_errors["Error: file contains illegal character"] = True
+
 
         # Miscellaneuos errors
         except Exception as e:
-            print("Unexpected error: {}".format(e))
+            self.__file_errors["Error: Unexpected error in file {}".format(e)] = True
