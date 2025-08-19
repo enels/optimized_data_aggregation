7f93e82519cb54de1624110392d667ec25e7e30a
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-19 10:33:42 +0100

    ingest data into database

--- utilities/load_data.py
+++ utilities/load_data.py
@@ -5,15 +5,35 @@ from db_connect import DBConnect
 class LoadData(DBConnect):
 
     
-    def __init__(self, data):
+    def __init__(self, data, table_name):
 
         super().__init__()
 
         # validated data list to be loaded into database
         self.__data = data
+        self.__table_name = table_name
+
 
     def load_data_to_dbase(self):
-        pass
+
+        """
+           Loads data into the database
+        """
+
+        # get column name
+        super().connect()
+
+        # query to get the column names
+        queries = self.__generate_queries()
+
+        cur = self.conn.cursor()
+
+        for query in queries:
+            cur.execute(query)
+            self.conn.commit()
+
+        self.conn.close_connection()
+
 
     def __get_product_id(self, product_name):
         pass
@@ -23,3 +43,49 @@ class LoadData(DBConnect):
     
     def __get_country_id(self, country):
         pass
+
+    def __generate_queries(self, id_value=None):
+
+        """
+          Generates queries to be executed and use to load data into database
+        """
+
+        generated_queries = list()
+
+        # extract the values in string format
+        values = self.__extract_values()
+
+        # if the id value is not set, then an insert query to be be generated 
+        if id_value == None:
+           # generate insert query
+
+           if self.__table_name == 'test_tbl':
+		# generates queries to insert into sales table
+              generated_queries.append("INSERT INTO {} (name, levy) VALUES{}".\
+format(self.__table_name, values))
+
+           else:
+              pass
+              # generates queries to insert into products table
+
+        else:
+              pass
+           # generates select query
+	
+        return generated_queries
+              
+    def __extract_values(self):
+        
+        # extract the values from the data in string format
+        values = ""
+        values_length = len(self.__data)
+        values_count = 0
+        
+        for value in self.__data:
+            if values_count != values_length - 1:
+                values += str(value) + ", "
+            else:
+                values += str(value)
+            values_count += 1
+
+        return values
