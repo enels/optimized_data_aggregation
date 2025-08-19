d6e4a1a7689149bd355d5604e1b2888519b57b3b
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-19 13:31:49 +0100

    fix the error check condition indentation

--- ingest_data.py
+++ ingest_data.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     """
 
     #file_path = "data/products_data.csv"
-    file_path = "data/test_tbl.csv"
+    file_path = "data/products.csv"
 
     
     ########## checks for any error in file ############
@@ -26,61 +26,60 @@ if __name__ == "__main__":
 
     "get the file name"
     file_name = status.get_file_name()
-    print(file_name)
 
     # check for any error(s) in file
     file_errors = status.get_file_errors()
+
+    #validation_errors = dict()
     
     # if there were file errors, skips file validation process
     if len(file_errors) > 0:
-       for k,v in file_errors.items():
-          print(k)
+        for k,v in file_errors.items():
+            print(k)
     else:
-       ########### validate data in file #############
-       data_handler = ValidateData(file_path)
+        ########### validate data in file #############
+        data_handler = ValidateData(file_path)
 
-       # first, open file for validation
-       data_handler.open_file_for_validation()
+        # first, open file for validation
+        data_handler.open_file_for_validation()
 
-       # validate number of columns in the csv file
-       data_handler.validate_num_of_columns()
+        # validate number of columns in the csv file
+        data_handler.validate_num_of_columns()
 
-       # validate the column names
-       # this checks if the column names in the csv file matches
-       # what is in the database
-       data_handler.validate_correct_header_names()
+        # validate the column names
+        # this checks if the column names in the csv file matches
+        # what is in the database
+        data_handler.validate_correct_header_names()
 
-       # checks if the entire data validation process was successful
-       # if it didn't, program raises an exception
-       validation_errors = data_handler.get_validation_errors()
+        # checks if the entire data validation process was successful
+        # if it didn't, program raises an exception
+        validation_errors = data_handler.get_validation_errors()
     
-       # validate the consistency of the delimiter
-       data_handler.validate_delimiter_consistency()
+        # validate the consistency of the delimiter
+        #data_handler.validate_delimiter_consistency()
 
-       # validate the datatype of the various data on the columns
-       # of the csv file
-       #data_handler.validate_datatype()
+        # validate the datatype of the various data on the columns
+        # of the csv file
+        #data_handler.validate_datatype()
 
 
-       # if none of fthe above validation failes, get the validated data
-       # Note that validated data is in List of Tuples
-       validated_data = data_handler.get_validated_data()
+        # if none of fthe above validation failes, get the validated data
+        # Note that validated data is in List of Tuples
+        validated_data = data_handler.get_validated_data()
 
-    # checks if there were errors in file validation process,skips data loading
-    if len(validation_errors) > 0:
-       for k,v in validation_errors.items():
-          print(k.format(file_name))
-       raise Exception ("Data was not loaded into database due to validation error(s).")
-    else:
-       print("...load data")
+        # checks if there were errors in file validation process,skips data loading
+        if len(validation_errors) > 0:
+            for k,v in validation_errors.items():
+                print(k.format(file_name))
 
-    ########## Load Validated Data Into Database ###################
+        ########## Load Validated Data Into Database ###################
 
-    # create the object to load the validated data into the database
-    table_name = file_name.split(".")[0]
-    column_names = data_handler.get_column_names()
+        else:
+            # create the object to load the validated data into the database
+            table_name = file_name.split(".")[0]
+            column_names = data_handler.get_column_names()
 
-    validated_data_handler = LoadData(validated_data, column_names, table_name)
+            validated_data_handler = LoadData(validated_data, column_names, table_name)
 
-    # load data into database
-    validated_data_handler.load_data_to_dbase()
+            # load data into database
+            validated_data_handler.load_data_to_dbase()
