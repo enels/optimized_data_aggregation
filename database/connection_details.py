655862a651a8ea84f0125d63b867599ec03a5427
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-20 11:42:53 +0100

    add database connection details

---
+++ database/connection_details.py
@@ -0,0 +1,8 @@
+'''
+    This file contains the database connection details to a postgres database
+    Change the values below to what corresponds to your own database
+'''
+
+db_name="shopdb"    # change this to your database
+db_user="postgres"  # change this to your database user name
+db_password="9@$$w07D" # change this to your database password
