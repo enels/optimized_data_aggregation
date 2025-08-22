4e2768bb1dc2c0a957b8dc5d56d1e8ba4cb1f8e1
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-22 21:53:33 +0100

    add docstring comment

--- db_connect.py
+++ db_connect.py
@@ -1,20 +1,21 @@
-# import the necessary files
 import psycopg2, database.connection_details as connection_details
-
-"""
-connection_details: contains the hard coded details of the database connection to be established
-psycopg2: helps in establishing the database connection
-"""
 
 class DBConnect:
 
     def __init__(self):
+        """
+            Initialize the various connection parameters
+        """
         self.db_name = connection_details.db_name # database name
         self.db_user = connection_details.db_user # database user
         self.db_password = connection_details.db_password # database password
         self.conn = ""
 
-    def connect(self):
+    def connect(self) -> bool:
+        """Returns True
+        
+            Initiate a connection to the database using the initialized connection parameters
+        """
 
         try:
             self.conn = psycopg2.connect("dbname={0} user={1} password={2}".format(self.db_name, self.db_user, self.db_password))
@@ -25,6 +26,10 @@ class DBConnect:
         return True
 
     def close_connection(self):
+        """Returns True
+
+            Closes a connection
+        """
         self.conn.close()
         
         return True
