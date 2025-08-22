f3845aa406a2aef2389023792465946e51ec4770
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-22 11:33:19 +0100

    test the monthly sales summaries sql query

--- query_performance_test_module.py
+++ query_performance_test_module.py
@@ -1,19 +1,34 @@
-import unitttest
+import unittest
 import psycopg2
 import time
-import db_connect
+from db_connect import DBConnect
 
 
 class TestQueryPerformance(unittest.TestCase):
     def setUp(self):
 
-            # connet to the database
-            self.conn = DBconnect
-            self.cursor = self.conn.connect().cursor()
+        # connet to the database
+        self.conn = DBConnect()
+        self.conn.connect()
+
+        self.cursor = self.conn.conn.cursor()
 
     def tearDown(self):
-            self.conn.close()
+        self.conn.close_connection()
 
+    def test_monthly_sales_summary_query_performance(self):
+            
+        query = "SELECT EXTRACT (MONTH FROM date) AS month, region_name, COUNT(product_id) * products.price AS \
+                 total_sales, 20 product_name FROM sales LEFT JOIN products USING(product_id) LEFT JOIN \
+                 regions USING (region_id) GROUP BY (month, products.price, product_name, region_name) ORDER BY month ASC;"
+
+        start_time = time.time()
+        self.cursor.execute(query)
+        self.cursor.fetchall()
+        end_time = time.time()
+        execution_time = end_time - start_time
+
+        self.assertLess(execution_time, 0.01)
 
 
 if __name__ == '__main__':
