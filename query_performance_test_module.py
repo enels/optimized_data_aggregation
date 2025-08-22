094d0bcf686baef9d44a93636f4f3193fb54d7bf
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-22 11:51:51 +0100

    output the execution time

--- query_performance_test_module.py
+++ query_performance_test_module.py
@@ -19,7 +19,7 @@ class TestQueryPerformance(unittest.TestCase):
     def test_monthly_sales_summary_query_performance(self):
             
         query = "SELECT EXTRACT (MONTH FROM date) AS month, region_name, COUNT(product_id) * products.price AS \
-                 total_sales, 20 product_name FROM sales LEFT JOIN products USING(product_id) LEFT JOIN \
+                 total_sales, product_name FROM sales LEFT JOIN products USING(product_id) LEFT JOIN \
                  regions USING (region_id) GROUP BY (month, products.price, product_name, region_name) ORDER BY month ASC;"
 
         start_time = time.time()
@@ -28,7 +28,9 @@ class TestQueryPerformance(unittest.TestCase):
         end_time = time.time()
         execution_time = end_time - start_time
 
-        self.assertLess(execution_time, 0.01)
+        print("Execution time: {:.4f}".format(execution_time))
+
+        self.assertLess(execution_time, 0.001)
 
 
 if __name__ == '__main__':
