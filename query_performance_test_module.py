ac07ecadf3987216d0ed4e4bce76b57eda7a2ecf
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-22 10:01:53 +0100

    setup testing

--- query_performance_test_module.py
+++ query_performance_test_module.py
@@ -1,3 +1,20 @@
 import unitttest
 import psycopg2
 import time
+import db_connect
+
+
+class TestQueryPerformance(unittest.TestCase):
+    def setUp(self):
+
+            # connet to the database
+            self.conn = DBconnect
+            self.cursor = self.conn.connect().cursor()
+
+    def tearDown(self):
+            self.conn.close()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
