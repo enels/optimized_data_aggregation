9ab449be4b382a68b444381d78f72c84d051dc53
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-08-22 12:00:36 +0100

    add the count product aggregation function to the query

--- query_performance_test_module.py
+++ query_performance_test_module.py
@@ -33,5 +33,22 @@ class TestQueryPerformance(unittest.TestCase):
         self.assertLess(execution_time, 0.001)
 
 
+    def test_monthly_sales_summary_query_performance(self):
+            
+        query = "SELECT EXTRACT (MONTH FROM date) AS month, region_name,COUNT(product_id) AS quantity_sold, \
+                COUNT(product_id) * products.price AS total_sales, product_name FROM sales LEFT JOIN products \
+                USING(product_id) LEFT JOIN regions USING (region_id) GROUP BY \
+                (month, products.price, product_name, region_name) ORDER BY month ASC;"
+
+        start_time = time.time()
+        self.cursor.execute(query)
+        self.cursor.fetchall()
+        end_time = time.time()
+        execution_time = end_time - start_time
+
+        print("Execution time: {:.4f}".format(execution_time))
+
+        self.assertLess(execution_time, 0.001)
+
 if __name__ == '__main__':
     unittest.main()
